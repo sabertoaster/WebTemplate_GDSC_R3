6e596d3c4d283acb5c6d404777d56c275964685b
sabertoaster <62453787+sabertoaster@users.noreply.github.com>
2023-11-01 20:50:02 +0700

    Almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'navigate-bar': '#7dd4a7',
+        'main-text-color': '#236719',
+        'primary-text-color': '#90a88c',
+        'base-color': '#e6e8e6',
+        'form-color': '#b2d3c2',
+      },
+      height: {
+        '124': '35rem',
+      }
+    },
   },
   plugins: [],
 }
